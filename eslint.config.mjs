2a4f85fbcd20cb7fada4a15aff22ae3887403ab6
Mahmoud Alaa <mahmohamed@2p.com.sa>
2025-10-07 16:03:45 +0300

    Add eslint-plugin-react to enhance linting for React components

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,20 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   js.configs.recommended,
+  {
+  ...reactPlugin.configs.flat.recommended,
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  },
   {
     files: ["**/*.js"],
     languageOptions: {
