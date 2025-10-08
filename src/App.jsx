5efc1f6e0d55179b8a95209e4641e0ae6259a7f7
Mahmoud Alaa <mahmohamed@2p.com.sa>
2025-10-08 15:59:18 +0300

    feat: update Pizza component to include image rendering and enhance App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,25 @@ import Pizza from "./Pizza";
 const App = () => {
   return (
     <div>
-    <h1>Padre Gino's - Order Now</h1>
-    <Pizza name=" Pepperoni" description="pep, chesse"/>
-    <Pizza name=" Hawaiian" description="ham, pinapple"/>
-    <Pizza name=" Veggie" description="mushrooms, peppers, onions"/>
+      <h1>Padre Gino's - Order Now</h1>
+      <Pizza
+        name=" Pepperoni"
+        description="pep, chesse"
+        img="/public/pizzas/pepperoni.webp"
+      />
+      <Pizza
+        name=" Hawaiian"
+        description="ham, pinapple"
+        img="/public/pizzas/hawaiian.webp"
+      />
+      <Pizza
+        name=" Veggie"
+        description="mushrooms, peppers, onions"
+        img="/public/pizzas/veggie.webp"
+      />
     </div>
-    
-  )
+  );
 };
-const container  = document.getElementById("root");
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -3,6 +3,8 @@ const Pizza = (props) => {
     <div>
       <h1>{props.name}</h1>
       <p>{props.description}</p>
+      <img src={props.image} alt={props.name} />
+
     </div>
     
   );
