7a5d247724d651d7f5a728435b34fcc8fa110534
mahmoudmohamedalaa <mahmoud.mo.alaa1999@gmail.com>
2025-11-14 15:26:06 +0200

    fix: correct syntax for QueryClient instantiation in App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import {routeTree} from './routeTree.gen'
 const router = createRouter({
   routeTree,
 });
-const queryClient = new QueryClient(
+const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       experimental_prefetchInrender: true,
     },
   },
-);
+});
 
 const App = () => {
   return (
